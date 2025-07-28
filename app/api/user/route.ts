b4f6ba94c746d99dcab5cd29336d5f9d438a0519
Mallikarjun <skollermallikarjun@gmail.com>
2025-07-28 23:01:07 +0530

    fixing user/route.ts

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,18 +3,18 @@ import { findUser } from "@/lib/db";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req:NextRequest,res:NextResponse){
-    const session = await getServerSession(authOptions);
-    if(!session?.user)
-    {
-        return NextResponse.json({error : "Unauthorised"},{status:401})
-    }
-    const user = await findUser(session.user.id)
-    if(user)
-    {
-        return NextResponse.json({user: user},{status:200})
-    }
-    else {
-        return NextResponse.json({error: "User not found"},{status:404})
-    }
+export async function GET(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const user = await findUser(session.user.id);
+
+  if (user) {
+    return NextResponse.json({ user }, { status: 200 });
+  } else {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
 }
